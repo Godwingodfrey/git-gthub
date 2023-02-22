0268c63c9630c853973e9898d722dfb98e9d9746
Godwin Godfrey <chiwins666@gmail.com>
2023-02-22 17:20:55 +0100

    First release of hello world

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("This is the first file in my new Git Repo.");
+    }
+}
