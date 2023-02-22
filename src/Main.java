ccd0727a2d63cb0722100696ed371fc926ad8838
Godwin Godfrey <chiwins666@gmail.com>
2023-02-22 18:35:25 +0100

    added another image

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("A new line in our file!");
         System.out.println("Added an image");
+        System.out.println("Added another image");
     }
 }
