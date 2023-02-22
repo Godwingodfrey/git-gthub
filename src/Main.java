22f7e6b0b3c98ad0037b0e77aaaaf8ba5ec6ffdb
Godwin Godfrey <chiwins666@gmail.com>
2023-02-22 18:26:19 +0100

    update my Main class

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("This is the first file in my new Git Repo.");
+        System.out.println("This line is here to show how merging works.");
     }
 }
